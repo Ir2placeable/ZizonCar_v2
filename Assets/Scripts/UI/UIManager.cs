37596f8a3c2cd6cdddbbd2f02043163760ba4276
Doeun <doeuncow@naver.com>
2017-12-14 04:40:18 +0900

    IG-MAIN에 UI 프리팹 넣음

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -7,22 +7,14 @@ public class UIManager : MonoBehaviour
 
     public void LeftClick()
     {
-        GetComponent("DrivingModule").FixedUpdate();
+     
     }
 
     public void RightClick()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            Debug.Log("Pressed RightArrow");
-        }
     }
 
     public void SkillClick()
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            Debug.Log("Pressed Skill");
-        }
     }
 }
