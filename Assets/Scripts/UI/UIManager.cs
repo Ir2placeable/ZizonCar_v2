efab4fb4c1cd41b99a2f81004a5569f07576ce3c
Doeun <doeuncow@naver.com>
2017-12-14 03:54:11 +0900

    UI제작중~~(미완성)

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -2,20 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIManager : MonoBehaviour {
+public class UIManager : MonoBehaviour
+{
 
-    public void LeftClick ()
+    public void LeftClick()
     {
-            
+        GetComponent("DrivingModule").FixedUpdate();
     }
 
     public void RightClick()
     {
-
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("Pressed RightArrow");
+        }
     }
 
     public void SkillClick()
     {
-
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Pressed Skill");
+        }
     }
 }
