508c88ed0dcef0ea5f30db17a72ce6647ad13f6c
ysck7300 <ysck73@naver.com>
2017-12-13 22:55:30 +0900

    VehicleSpawn 기능 추가.

---
+++ Assets/Scripts/VehicleSpawn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleSpawn : MonoBehaviour {
+
+    public GameObject[] vehiclePrefab = null;
+    public GameObject[] spawnZone = null;
+
+    private GameObject playZone = null;
+
+	// Use this for initialization
+	void Start () {
+        playZone = GameObject.Find("PlayZone");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private GameObject CreateVehicle(int vehicleIndex)
+    {
+        GameObject createdVehicle = Instantiate(vehiclePrefab[vehicleIndex], spawnZone[vehicleIndex].transform.position, spawnZone[vehicleIndex].transform.rotation, playZone.transform);
+        return createdVehicle;
+    }
+
+
+    public void Spawn()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            GameObject spawnedVehicle = CreateVehicle(i);
+        }
+            
+    }
+}
